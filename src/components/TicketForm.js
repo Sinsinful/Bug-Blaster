fddb785258bdbf434edf307fe15f82b3daae4214
Sinsinful <sin_1210@hotmail.com>
2025-04-13 16:42:53 +0100

    Added edit, delete, sort. Refactored for reducer

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function TicketForm() {
+export default function TicketForm({ dispatch, editingTicket }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("1");
+
+  useEffect(() => {
+    if (editingTicket) {
+      setTitle(editingTicket.title);
+      setDescription(editingTicket.description);
+      setPriority(editingTicket.priority);
+    } else {
+      clearForm();
+    }
+  }, [editingTicket]);
 
   const priorityLabels = {
     1: "Low",
@@ -21,12 +31,21 @@ export default function TicketForm() {
     e.preventDefault();
 
     const ticketData = {
-      id: new Date().toISOString(),
+      id: editingTicket ? editingTicket.id : new Date().toISOString(),
       title,
       description,
       priority,
     };
+    dispatch({
+      type: editingTicket ? "UPDATE_TICKET" : "ADD_TICKET",
+      payload: ticketData,
+    });
 
+    clearForm();
+  };
+
+  const handleCancelEdit = () => {
+    dispatch({ type: "CLEAR_EDITING_TICKET" });
     clearForm();
   };
 
@@ -69,6 +88,12 @@ export default function TicketForm() {
       <button type="submit" className="button">
         Submit
       </button>
+
+      {editingTicket && (
+        <button className="button" onClick={handleCancelEdit}>
+          Cancel Edit
+        </button>
+      )}
     </form>
   );
 }
